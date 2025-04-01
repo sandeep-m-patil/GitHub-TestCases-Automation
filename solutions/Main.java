f417db8a3cbcbc6b58676f49ad032725c8906d11
sandeep <sandeeppatil10705@gmail.com>
2025-04-01 20:10:15 +0530

    modified java

--- solutions/Main.java
+++ solutions/Main.java
@@ -22,7 +22,6 @@ public class Main {
         return longest;
 
     }
-
     public static void main(String[] args) {
         System.out.println("Main class compiled successfully. Run MainTest.java for tests.");
     }
