b867240f87cfadd11e6fed72b077f6a31aafd493
Sandeep Madhavrao Patil <85793480+sandeep-m-patil@users.noreply.github.com>
2025-04-30 13:11:22 +0530

    Update main.c

--- main/main.c
+++ main/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 // Function to find the longest mountain in an array..
 int longestMountain(int arr[], int n)
-{
-    // code for Longest Mountain
+{ int longest = 0;
+    for (int i = 1; i < arrSize - 1; i++) {
+        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+            int left = i;
+            while (left > 0 && arr[left] > arr[left - 1]) left--;
+            int right = i;
+            while (right < arrSize && arr[right] > arr[right + 1]) right++;  // ❌ Error: Out of bounds access
+            int len = right - left + 1;
+            if (len > longest) longest = len;
+        }
+    }
+    return longest;
 }
